9b58632a574ce1403825ed3b9f4cbe00e35772b1
AMIEian <nitin4july@gmail.com>
2022-03-22 17:59:05 +0530

    Add project files.

---
+++ TagidPOS/TagidPOSHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TagidPOS
+{
+    public class TagidPOSHelper
+    {
+        public static void PutBilledBarcodes(List<string> barcodes)
+        {
+            Clipboard.Clear();
+            string data = "Tgid";
+            foreach(string barcode in barcodes)
+            {
+                data = data + barcode + ",";
+            }
+            data = data.Remove(data.Length - 1);
+            Clipboard.SetText(data);
+        }
+
+        public static void PutBilledBarcodes(string barcodes)
+        {
+            Clipboard.Clear();
+            Clipboard.SetText("Tagid" + barcodes);
+        }
+    }
+}
